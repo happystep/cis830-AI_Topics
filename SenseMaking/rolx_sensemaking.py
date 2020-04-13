8173a2ae5c12fce890b899f1d3b2b5155420e3aa
Luis Bobadilla <happystep@ksu.edu>
2020-04-13 09:26:18 -0500

    pep9

--- SenseMaking/rolx_sensemaking.py
+++ SenseMaking/rolx_sensemaking.py
@@ -1,7 +1,11 @@
-# This file will implement KDD 2012, sense making. 
+#  This file will implement KDD 2012, sense making
+#  This is to clarify the semantics of the 4 roles given by RolX in my thesis experiment
+
 
 '''
 Given graph G and node-role matrix H, returns a role-feature matrix K for sensemaking analyses of roles.
 '''
+
+
 def make_sense(G, H):
     pass
