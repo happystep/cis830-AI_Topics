5280da4249300930fff842451b49d2e46a860661
Luis Bobadilla <happystep@ksu.edu>
2020-04-13 09:24:05 -0500

    started make_sense function

---
+++ SenseMaking/rolx_sensemaking.py
@@ -0,0 +1,7 @@
+# This file will implement KDD 2012, sense making. 
+
+'''
+Given graph G and node-role matrix H, returns a role-feature matrix K for sensemaking analyses of roles.
+'''
+def make_sense(G, H):
+    pass
